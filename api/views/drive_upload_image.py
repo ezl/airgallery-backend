36244ca493b914a36d68bd57f8504b5688fc59ca
ezl <ericzliu@gmail.com>
2022-12-31 14:38:03 -0600

    rm empty space lines

--- api/views/drive_upload_image.py
+++ api/views/drive_upload_image.py
@@ -11,46 +11,46 @@ from googleapiclient.http import MediaIoBaseUpload
 class DriveUploadImage(APIView):
     def put(self, request):
         gallery = Gallery.objects.filter(user__id=request.user.id).prefetch_related('storage_backend').first()
-        
+
         if gallery is None:
             return Response('Default gallery not found', status=status.HTTP_404_NOT_FOUND)
-        
-        file = request.FILES['image'] 
-        
+
+        file = request.FILES['image']
+
         new_file = self.upload(gallery, file)
-        
+
         self.create_image(gallery, new_file)
-        
+
         return Response(data=new_file)
-    
+
     def upload(self, gallery, target_file):
         try:
             service = get_drive_service(
                 gallery.storage_backend.meta['access_token'],
                 gallery.storage_backend.meta['refresh_token'],
             )
-        
+
             file_metadata = {
                 'name': target_file.name,
                 'parents': [gallery.folder_id]
             }
-            
+
             f = io.BytesIO(target_file.read())
             media = MediaIoBaseUpload(f, mimetype=get_mime_type(f))
 
             file = service.files().create(
-                body=file_metadata, 
+                body=file_metadata,
                 media_body=media,
                 fields='id, name, imageMediaMetadata, mimeType, thumbnailLink'
             ).execute()
-        
+
             print(file)
-            
+
             return file
         except HttpError as error:
             print(F'An error occurred: {error}')
-            
-        
+
+
     def create_image(self, gallery, file):
         img = Image()
         img.name = file['name']
@@ -60,5 +60,5 @@ class DriveUploadImage(APIView):
         img.mime_type = file['mimeType']
         img.gallery = gallery
         img.save()
-        
+
         return img
