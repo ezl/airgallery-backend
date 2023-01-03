7ecb2e611ea8f1062de12f4fc2ac0543b71eef62
ezl <ericzliu@gmail.com>
2023-01-02 21:22:29 -0600

    Rm trailing whitespace

--- api/helpers.py
+++ api/helpers.py
@@ -13,9 +13,9 @@ def get_drive_service(access_token, refresh_token):
         client_id= env('GOOGLE_CLIENT_ID'),
         client_secret= env('GOOGLE_CLIENT_SECRET'),
     )
-    
+
     service = build('drive', 'v3', credentials=creds)
-    
+
     return service
 
 
@@ -31,21 +31,21 @@ def get_mime_type(file):
 
 def fetchGalleryImages(gallery):
     """
-    Retrieve image files from this gallery’s folder 
+    Retrieve image files from this gallery’s folder
     """
-    
+
     service = get_drive_service(
         gallery.storage_backend.meta['access_token'],
         gallery.storage_backend.meta['refresh_token'],
     )
-    
+
     query = f"parents = '{gallery.folder_id}'"
     fields = 'files(id, name, imageMediaMetadata, thumbnailLink)'
 
     res = service.files().list(q=query, fields=fields).execute()
-    
+
     files = res.get('files', [])
-    
+
     return files
 
 def drive_create_folder(name, access_token, refresh_token, parent=None):
@@ -58,13 +58,13 @@ def drive_create_folder(name, access_token, refresh_token, parent=None):
         'name': name,
         'mimeType': 'application/vnd.google-apps.folder',
     }
-    
+
     if parent is not None:
         file_metadata['parents'] = [parent]
-    
+
     folder = service.files().create(body=file_metadata, fields='id').execute()
-    
+
     print('Created folder for the new gallery:')
     print(folder)
-    
+
     return folder['id']
