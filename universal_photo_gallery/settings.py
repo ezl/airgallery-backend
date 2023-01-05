d3c9a5ce9ead50e1de7fababce03d927b0ff019b
ezl <ericzliu@gmail.com>
2023-01-04 19:06:12 -0600

    update: permission = IsAuthenticatedOrReadOnly

--- universal_photo_gallery/settings.py
+++ universal_photo_gallery/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ]
 }
 
