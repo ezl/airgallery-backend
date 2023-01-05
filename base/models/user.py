0373ff77c6b826a8d0c326908c4fa887a7a5deda
ezl <ericzliu@gmail.com>
2023-01-04 19:13:05 -0600

    Rm whitespace

--- base/models/user.py
+++ base/models/user.py
@@ -8,10 +8,10 @@ class UserManager(BaseUserManager):
             return ValueError('Email is missing')
 
         user = self.model(email=self.normalize_email(email))
-        
+
         if password is None:
             user.set_password(password)
-            
+
         user.save(uaing=self._db)
 
         return user
@@ -25,11 +25,11 @@ class User(AbstractBaseUser):
     auth_provider_user_id = models.CharField(max_length=250)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     USERNAME_FIELD = 'email'
 
     objects = UserManager()
-    
+
     class Meta():
         constraints = [
             models.UniqueConstraint(
@@ -37,8 +37,6 @@ class User(AbstractBaseUser):
                 name='users.auth_provider_name_auth_provider_user_id_unique'
             )
         ]
-
-
 
 def find_or_create_user(auth_provider_name, user_data):
     user = User.objects.filter(
