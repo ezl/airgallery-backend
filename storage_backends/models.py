9211316a4e2108db75f08d0922a406e766232c8b
ezl <ericzliu@gmail.com>
2023-01-07 10:53:14 -0600

    remove timezone import
    don't need because we are using TimeStampedModel

--- storage_backends/models.py
+++ storage_backends/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
 
 from django_extensions.db.models import TimeStampedModel
+
 
 class StorageBackend(TimeStampedModel):
     name = models.CharField(max_length=255)
