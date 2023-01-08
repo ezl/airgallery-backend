f683b45cb995df195cd082a86e3ac1d831fc662c
ezl <ericzliu@gmail.com>
2023-01-08 11:41:57 -0600

    move drf urlpatterns into separate path
    temporary for now. so it's easier to copy all the original endpoints

---
+++ universal_photo_gallery/drf_urls.py
@@ -0,0 +1,15 @@
+from rest_framework import routers
+
+from galleries.views import GalleryViewSet, ImageViewSet
+from user_profiles.views import UserViewSet
+from storage_backends.views import StorageBackendViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'galleries', GalleryViewSet)
+router.register(r'images', ImageViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'storage_backends', StorageBackendViewSet)
+
+urlpatterns = router.urls
+
--- universal_photo_gallery/urls.py
+++ universal_photo_gallery/urls.py
@@ -1,23 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework import routers
-
-from galleries.views import GalleryViewSet, ImageViewSet
-from user_profiles.views import UserViewSet
-from storage_backends.views import StorageBackendViewSet
+from .drf_urls import urlpatterns as drf_urlpatterns
 
 
-router = routers.DefaultRouter()
-router.register(r'galleries', GalleryViewSet)
-router.register(r'images', ImageViewSet)
-router.register(r'users', UserViewSet)
-router.register(r'storage_backends', StorageBackendViewSet)
-
-urlpatterns = router.urls
-
-urlpatterns += [
+urlpatterns = [
     path('/', include("base.urls")),
     path('api/', include("api.urls")),
+    path('drf/', include(drf_urlpatterns)),
     path('admin/', admin.site.urls),
 ]
