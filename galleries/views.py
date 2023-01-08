1f80f813bc7de63afea2bf7dbc7137aafca28b9c
ezl <ericzliu@gmail.com>
2023-01-08 13:41:54 -0600

    look up galleries by slug not id

--- galleries/views.py
+++ galleries/views.py
@@ -9,6 +9,7 @@ class GalleryViewSet(viewsets.ModelViewSet):
     queryset = Gallery.objects.all()
     serializer_class = GallerySerializer
     permission_classes = [permissions.AllowAny]
+    lookup_field = 'slug' #TODO: rename to uuid, this field isn't actually a slug
 
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Image.objects.all()
